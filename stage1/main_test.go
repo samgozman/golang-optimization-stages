210891dec5c38621d8096cc8bccbeb6b3e515c4e
samgozman <sam@gozman.space>
2024-05-05 10:22:01 +0400

    chore

--- stage1/main_test.go
+++ stage1/main_test.go
@@ -12,19 +12,6 @@ import (
 // Usage:
 //
 //	go test -v -cpuprofile=cpu.pprof -memprofile=mem.pprof -benchmem -bench=. -benchtime=1s -count=10
-//
-// Results:
-//
-//	BenchmarkServeApp-10                  75          15359607 ns/op         5022236 B/op      63424 allocs/op
-//	BenchmarkServeApp-10                  79          15283220 ns/op         5019885 B/op      63420 allocs/op
-//	BenchmarkServeApp-10                  80          15376617 ns/op         5019731 B/op      63421 allocs/op
-//	BenchmarkServeApp-10                  81          15324918 ns/op         5018524 B/op      63418 allocs/op
-//	BenchmarkServeApp-10                  81          15241640 ns/op         5017535 B/op      63417 allocs/op
-//	BenchmarkServeApp-10                  80          15320955 ns/op         5017719 B/op      63417 allocs/op
-//	BenchmarkServeApp-10                  81          15473488 ns/op         5016617 B/op      63417 allocs/op
-//	BenchmarkServeApp-10                  80          15434468 ns/op         5016502 B/op      63416 allocs/op
-//	BenchmarkServeApp-10                  74          15740035 ns/op         5015688 B/op      63416 allocs/op
-//	BenchmarkServeApp-10                  76          15920205 ns/op         5015834 B/op      63414 allocs/op
 func BenchmarkServeApp(b *testing.B) {
 	// Start pprof profiling
 	if err := utils.StartPprof(); err != nil {
--- stage2/main_test.go
+++ stage2/main_test.go
@@ -12,19 +12,6 @@ import (
 // Usage:
 //
 //	go test -v -cpuprofile=cpu.pprof -memprofile=mem.pprof -benchmem -bench=. -benchtime=1s -count=10
-//
-// Results:
-//
-//	BenchmarkServeApp-2           74          16027115 ns/op         5014060 B/op      63421 allocs/op
-//	BenchmarkServeApp-2           66          15508486 ns/op         5013355 B/op      63418 allocs/op
-//	BenchmarkServeApp-2           73          15658756 ns/op         5013058 B/op      63418 allocs/op
-//	BenchmarkServeApp-2           70          15703708 ns/op         5012548 B/op      63415 allocs/op
-//	BenchmarkServeApp-2           78          15987481 ns/op         5012574 B/op      63414 allocs/op
-//	BenchmarkServeApp-2           80          15780026 ns/op         5012611 B/op      63413 allocs/op
-//	BenchmarkServeApp-2           79          16164028 ns/op         5012219 B/op      63413 allocs/op
-//	BenchmarkServeApp-2           68          15807772 ns/op         5012287 B/op      63410 allocs/op
-//	BenchmarkServeApp-2           79          16032428 ns/op         5011579 B/op      63411 allocs/op
-//	BenchmarkServeApp-2           72          15638314 ns/op         5011674 B/op      63409 allocs/op
 func BenchmarkServeApp(b *testing.B) {
 	// Start pprof profiling
 	if err := utils.StartPprof(); err != nil {
